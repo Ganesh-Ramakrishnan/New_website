3ec6167a83534827a995b6188ccae8e2f0b13e2d
ganesh.r <ganesh.r@simplify3x.com>
2025-10-06 09:34:52 +0530

    latest updates

--- src/components/CircularWorkflow.tsx
+++ src/components/CircularWorkflow.tsx
@@ -132,43 +132,45 @@ const CircularWorkflow: React.FC = () => {
   const centerX = 265;
   const centerY = 265;
 
-  const renderIcon = (iconType: string, x: number, y: number, isActive: boolean) => {
+  const renderIcon = (iconType: string, x: number, y: number, isActive: boolean, isCenter: boolean = false) => {
     const color = "#ffffff";
+    const size = isCenter ? 40 : 20;
+    const halfSize = size / 2;
     
     switch (iconType) {
       case 'web':
         return (
-          <svg x={x - 10} y={y - 10} width="20" height="20" viewBox="0 0 24 24" fill={color}>
+          <svg x={x - halfSize} y={y - halfSize} width={size} height={size} viewBox="0 0 24 24" fill={color}>
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
           </svg>
         );
       case 'mobile':
         return (
-          <svg x={x - 10} y={y - 10} width="20" height="20" viewBox="0 0 24 24" fill={color}>
+          <svg x={x - halfSize} y={y - halfSize} width={size} height={size} viewBox="0 0 24 24" fill={color}>
             <path d="M17 2H7c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H7V6h10v10z"/>
           </svg>
         );
       case 'api':
         return (
-          <svg x={x - 10} y={y - 10} width="20" height="20" viewBox="0 0 24 24" fill={color}>
+          <svg x={x - halfSize} y={y - halfSize} width={size} height={size} viewBox="0 0 24 24" fill={color}>
             <path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/>
           </svg>
         );
       case 'enterprise':
         return (
-          <svg x={x - 10} y={y - 10} width="20" height="20" viewBox="0 0 24 24" fill={color}>
+          <svg x={x - halfSize} y={y - halfSize} width={size} height={size} viewBox="0 0 24 24" fill={color}>
             <path d="M12 7V3H2v18h20V7H12zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10zm-2-8h-2v2h2v-2zm0 4h-2v2h2v-2z"/>
           </svg>
         );
       case 'desktop':
         return (
-          <svg x={x - 10} y={y - 10} width="20" height="20" viewBox="0 0 24 24" fill={color}>
+          <svg x={x - halfSize} y={y - halfSize} width={size} height={size} viewBox="0 0 24 24" fill={color}>
             <path d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7l-2 3v1h8v-1l-2-3h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 12H3V4h18v10z"/>
           </svg>
         );
       case 'database':
         return (
-          <svg x={x - 10} y={y - 10} width="20" height="20" viewBox="0 0 24 24" fill={color}>
+          <svg x={x - halfSize} y={y - halfSize} width={size} height={size} viewBox="0 0 24 24" fill={color}>
             <path d="M12 3C7.58 3 4 4.79 4 7s3.58 4 8 4 8-1.79 8-4-3.58-4-8-4zM4 9v3c0 2.21 3.58 4 8 4s8-1.79 8-4V9c0 2.21-3.58 4-8 4s-8-1.79-8-4zm0 5v3c0 2.21 3.58 4 8 4s8-1.79 8-4v-3c0 2.21-3.58 4-8 4s-8-1.79-8-4z"/>
           </svg>
         );
@@ -320,14 +322,19 @@ const CircularWorkflow: React.FC = () => {
                 strokeWidth="1"
               />
 
-              {/* SimplifyQA Logo in center */}
+              {/* SimplifyQA Logo - positioned at top of center */}
               <image
                 href="/simplify_logo.svg"
-                x={centerX - 60}
-                y={centerY - 60}
-                width="120"
-                height="120"
+                x={centerX - 50}
+                y={centerY - 80}
+                width="100"
+                height="100"
               />
+              
+              {/* Active section icon in center - positioned in the middle */}
+              <g className="transition-all duration-500 ease-in-out">
+                {renderIcon(workflowSections[activeSection]?.icon, centerX, centerY + 20, true, true)}
+              </g>
 
               {/* Icon Circles - positioned on the outer circle line */}
               {workflowSections.map((section, index) => {
